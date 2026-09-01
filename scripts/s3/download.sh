20026b8b1e5c9c9373e8b51cace4e1cdeac5b4a6
ComputelessComputer <63365510+ComputelessComputer@users.noreply.github.com>
2026-09-01 15:58:27 +0900

    Remove remaining Yujong-controlled dependencies

--- scripts/s3/download.sh
+++ scripts/s3/download.sh
@@ -3,7 +3,7 @@ ENDPOINT_URL="https://45207bf426d0acf3baf9f70fd2c610c8.r2.cloudflarestorage.com"
 BUCKET="anarlog-cache"
 
 FROM_PATH="s3://$BUCKET/v0/*"
-TO_PATH="/Users/yujonglee/dev/anarlog/.cache/"
+TO_PATH="$HOME/dev/anarlog/.cache/"
 
 AWS_REGION=auto s5cmd \
     --log trace \
